b6a8950eed9d8ca2afcc37901dc4041b753380be
Pinglei Guo <pingleig@amazon.com>
2022-03-17 20:44:35 -0700

    packer: Use different version name & Upgrade aws provider
    Have to create a new one because ami search for owner = self ...

--- terraform/main/main.tf
+++ terraform/main/main.tf
@@ -51,8 +51,17 @@ variable "metrics_ip" {
 }
 
 // PROVIDER/AWS ##############################
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "aws" {
-  version = "~> 2.49"
   region  = var.aws_region
 }
 
@@ -308,6 +317,7 @@ output "scheduler_dns" {
 }
 output "scheduler_pg_password" {
   value = module.scheduler.pg_password
+  sensitive = true
 }
 
 output "monitor_dns" {
